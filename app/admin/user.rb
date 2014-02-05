4e8b2ae2c05add96d536db7c6d234a02ebb5cd7d
phil <phil@branch14.org>
2014-02-05 11:03:16 +0100

    strip down index and form for users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,39 @@
 ActiveAdmin.register User do
 
-  
+  index do
+    column :id
+    column :firstname
+    column :lastname
+    column :email
+    column 'Venues' do |user|
+      user.venues.count
+    end
+    # column 'Talk Count' do |user|
+    #   # TODO link_to user.talks.count, admin_talks # by user
+    #   user.talks.count
+    # end
+    column :sign_in_count
+    column :last_sign_in_at do |user|
+      span style: 'white-space: pre'  do
+        l user.last_sign_in_at, format: :iso
+      end
+    end
+    column :created_at do |user|
+      span style: 'white-space: pre'  do
+        l user.created_at, format: :iso
+      end
+    end
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :firstname
+      f.input :lastname
+      f.input :email
+    end
+  end
+
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 
-  
+  has_many :venues
+  #has_many :talks, through: :venues
 
 end
