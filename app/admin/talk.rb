fd122173fdb57fae77db79a59ee8dca2f0f23e9c
phil <phil@branch14.org>
2014-02-13 10:18:55 +0100

    set strong parameters for talks

--- app/admin/talk.rb
+++ app/admin/talk.rb
@@ -50,17 +50,13 @@ ActiveAdmin.register Talk do
     f.actions
   end
   
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+  permit_params %w( title
+                    starts_at
+                    featured_from
+                    duration
+                    venue_id
+                    teaser
+                    description
+                    record ).map(&:to_sym)
   
 end
