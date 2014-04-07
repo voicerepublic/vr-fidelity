b3844a78c5f38eb9b64775e365277bd109e4d3b8
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-04-07 22:50:16 +0200

    delete unneeded code

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module VoicerepublicBackoffice
     # -- all .rb files in that directory are automatically loaded.
 
     config.autoload_paths += %W( #{config.root}/app/jobs )
-    
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -27,9 +27,5 @@ module VoicerepublicBackoffice
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # http://stackoverflow.com/questions/4707896/prefix-urls-in-rails-application
-    # RAILS_RELATIVE_URL_ROOT
-    config.relative_url_root = '/backend'
-    
   end
 end
