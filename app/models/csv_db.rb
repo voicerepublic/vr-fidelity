4a638467014948b6e3a460de6880303f49265655
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-05-31 19:51:00 +0200

    csv upload: brainfart

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -5,11 +5,16 @@ class CsvDb
       csv_file = csv_data.read
       target_model = model_name.classify.constantize
       csv = CSV.parse(csv_file, headers: true, quote_char: '"')
+      # validation
       csv.each_with_index do |row, i|
         obj = target_model.new(row.to_hash)
         unless obj.valid?
           return { error: "Error in line #{i+1}: #{obj.errors.full_messages.join(';')}" }
         end
+      end
+      # creation
+      csv.each do |row|
+        target_model.create!(row.to_hash)
       end
       return { success: csv.length }
     end
