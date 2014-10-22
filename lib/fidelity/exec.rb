f5da118d566aa1579cd67fa8fd8f14f5729c56db
phil <phil@branch14.org>
2014-10-22 19:02:51 +0200

    fix minor issues

--- lib/fidelity/exec.rb
+++ lib/fidelity/exec.rb
@@ -1,15 +1,19 @@
 module Fidelity
-  class Exec < Struct.new(:args)
+  class Exec < Struct.new(:file)
 
     class << self
       def run(args, logger=nil)
-        file = args.unshift
+        file = args.shift
         new(file).run(logger)
       end
     end
 
     def run(logger)
-      ChainRunner.new(file).run(logger)
+      path = File.dirname(file)
+      base = File.basename(file)
+      Dir.chdir(path) do
+        ChainRunner.new(base).run(logger)
+      end
     end
 
   end
