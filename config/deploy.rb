0dbd925b0e7941d324ce37ccffd390420c160098
phil <phil@branch14.org>
2014-10-27 11:07:40 +0100

    fix deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :deploy_to, '/home/backend/app'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/private_pub.yml}
+set :linked_files, %w{config/database.yml config/private_pub.yml config/settings.local.yml}
 
 # Default value for linked_dirs is []
 #set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
