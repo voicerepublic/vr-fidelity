e955f4086efc89d6c8e320461ba6ce61d4ee22bc
phil <phil@branch14.org>
2014-10-28 18:26:30 +0100

    refactor ConsoleLogger into something maintainable

--- lib/fidelity/console_logger.rb
+++ lib/fidelity/console_logger.rb
@@ -9,27 +9,36 @@ module Fidelity
 
     def method_missing(method, *args)
       return super unless METHODS.include?(method.to_s)
+
       arg = args.shift
-      prefix, suffix = nil, [reset, "\n"]
-      case method
-      when :debug then prefix = '    '
-      when :info  then prefix = ["\n", green, '--> ']
-      when :warn  then prefix = ['    ', red]
-      when :error then prefix = [red, bold]
-      when :fatal then prefix = [black, on_red]
-      when :unknown
-        # noop
-      else
-        raise "unknown severity level #{method}"
-      end
       arg.split("\n").each do |line|
-        subfix = nil
-        case line[0]
-        when '%' then subfix = cyan
-        end
-        parts = [prefix, subfix, line, suffix].flatten
-        print *parts
+        print *[ prefix[method],
+                 subfix[line[0]],
+                 line,
+                 suffix ].flatten
       end
+    end
+
+    def prefix
+      @prefix ||= {
+        debug:   '    ',
+        info:    [ "\n", green, '--> ' ],
+        warn:    [ '    ', red ],
+        error:   [ red, bold ],
+        fatal:   [ black, on_red ],
+        unknown: nil
+      }
+    end
+
+    # adjust formatting based on first char of line
+    def subfix
+      @subfix ||= {
+        '%' => cyan
+      }
+    end
+
+    def suffix
+      @suffix ||= [ reset, "\n" ]
     end
 
   end
