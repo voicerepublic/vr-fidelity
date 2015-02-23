de755e646e438217234dac5fb24a291100ee60a5
phil <phil@branch14.org>
2015-02-23 15:07:22 +0100

    cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,12 @@ module ApplicationHelper
 
   def metric(metric, fmt='%d')
     # apply some naming specific number formatting
-    fmt = '%.1f' if metric.key.match(/_percent$/)
+    fmt = '%.1f%' if metric.key.match(/_percent$/)
     fmt = '%.2f' if metric.key.match(/_per_|_ratio$/)
 
-    (t(".#{metric.key}", count: fmt % metric.value) +
-     link_to(metric.key, admin_metric_path(id: metric.key))).html_safe
+    content_tag(:e, fmt % metric.value) +
+      t('.'+metric.key) +
+      link_to(metric.key, admin_metric_path(id: metric.key))
   end
 
   alias_method :m, :metric
--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,5 +1,2 @@
 class Metric < ActiveRecord::Base
-
-  scope :latest, -> { order('created_at DESC').select(:key).distinct }
-
 end
