744d95e36d746ae5059f88313966ee3019065cbd
phil <phil@branch14.org>
2015-11-18 16:14:12 +0100

    add state suspended, which adds a scope automatically

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -26,7 +26,7 @@ class Talk < ActiveRecord::Base
 
   extend ::CsvImport
 
-  STATES = %w( pending prelive live postlive processing archived )
+  STATES = %w( pending prelive live postlive processing archived suspended )
 
   # TODO create a better more specific pattern for urls
   URL_PATTERN = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
