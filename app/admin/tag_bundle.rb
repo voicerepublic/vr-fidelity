73657d2830139d4acb6d28d7aa0553d2d640c8fe
phil <phil@branch14.org>
2015-11-25 14:10:41 +0100

    introduce groups on tag bundles

--- app/admin/tag_bundle.rb
+++ app/admin/tag_bundle.rb
@@ -2,15 +2,23 @@ ActiveAdmin.register TagBundle do
 
   menu priority: 21
 
+  TagBundle::GROUPS.each do |group|
+    scope group.to_sym
+  end
+
+  scope :standalone
+
   filter :title_en
   filter :title_de
+  filter :group
 
-  permit_params :title_en, :title_de, :tag_list
+  permit_params :title_en, :title_de, :group, :tag_list
 
   index do
     selectable_column
     column :title_en
     column :title_de
+    column :group
     column :tag_list
     actions
   end
@@ -19,6 +27,7 @@ ActiveAdmin.register TagBundle do
     f.inputs do
       f.input :title_en
       f.input :title_de
+      f.input :group, collection: TagBundle::GROUPS
       f.input :tag_list, input_html: { value: f.object.tag_list * ', ' }, label: "Tags"
     end
     f.actions
--- app/models/tag_bundle.rb
+++ app/models/tag_bundle.rb
@@ -1,5 +1,13 @@
 class TagBundle < ActiveRecord::Base
 
+  GROUPS = %w( category publisher format profession )
+
+  GROUPS.each do |group|
+    scope group.to_sym, -> { where(group: group) }
+  end
+
+  scope :standalone, -> { where(group: nil) }
+
   acts_as_taggable
 
 end
