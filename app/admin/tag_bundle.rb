8180634b19f1a59d12b7c77c8cffe178f66bbcd3
phil <phil@branch14.org>
2015-12-03 13:52:52 +0100

    add more fields to tag bundles

--- app/admin/tag_bundle.rb
+++ app/admin/tag_bundle.rb
@@ -11,24 +11,31 @@ ActiveAdmin.register TagBundle do
   filter :title_en
   filter :title_de
   filter :group
+  filter :promoted
 
-  permit_params :title_en, :title_de, :group, :tag_list
+  permit_params :title_en, :title_de,
+                :content_en, :content_de,
+                :group, :promoted, :tag_list
 
   index do
     selectable_column
     column :title_en
     column :title_de
     column :group
+    column :promoted
     column :tag_list
     actions
   end
 
   form do |f|
     f.inputs do
-      f.input :title_en
-      f.input :title_de
       f.input :group, collection: TagBundle::GROUPS
+      f.input :promoted, label: 'Promoted (Promoted categories are listed on the home page.)'
       f.input :tag_list, input_html: { value: f.object.tag_list * ', ' }, label: "Tags"
+      f.input :title_en
+      f.input :description_en, hint: 'Descriptions are customer facing texts. So choose you language wisely. ;)'
+      f.input :title_de
+      f.input :description_de
     end
     f.actions
   end
@@ -41,6 +48,5 @@ ActiveAdmin.register TagBundle do
       end
     end
   end
-
 
 end
