893ff2ed2ea109d9f7e69cfbb9af9b2beb1d32f1
phil <phil@branch14.org>
2015-12-09 10:03:21 +0100

    do not populate missing fields on new records

--- app/models/page.rb
+++ app/models/page.rb
@@ -37,7 +37,7 @@ class Page < ActiveRecord::Base
   accepts_nested_attributes_for :sections
 
   after_initialize :set_defaults, if: :new_record?
-  after_initialize :populate_missing
+  after_initialize :populate_missing, unless: :new_record?
 
   before_save :set_content
 
