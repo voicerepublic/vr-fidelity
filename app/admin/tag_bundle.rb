34f4cbd0174c7ffcc453a356434343e4e0bca702
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2016-01-07 16:06:35 +0100

    build csv export for talks in tag bundles

--- app/admin/tag_bundle.rb
+++ app/admin/tag_bundle.rb
@@ -31,6 +31,8 @@ ActiveAdmin.register TagBundle do
   form do |f|
     f.inputs do
       f.input :group, collection: TagBundle::GROUPS
+      f.input :icon, collection: ["arts", "tech", "media", "business",
+      "science", "politics"]
       f.input :promoted, label: 'Promoted (Promoted categories are listed on the home page.)'
       f.input :tag_list, input_html: { value: f.object.tag_list * ', ' }, label: "Tags"
       f.input :title_en
@@ -48,6 +50,24 @@ ActiveAdmin.register TagBundle do
         format.html { redirect_to admin_tag_bundles_path }
       end
     end
+  end
+
+  member_action :download_report, method: :get do
+    talks = Talk.tagged_with(TagBundle.find(params[:id]).tags, any: true)
+
+    csv = CSV.generate( encoding: 'Windows-1251', force_quotes: true ) do |csv|
+      csv << [ "play_count", "title", "speaker", "link", "series", "user" ]
+
+      talks.each do |talk|
+        csv << [ talk.play_count, talk.title, talk.speakers, "https://voicerepublic.com/talk/#{talk.slug}", talk.series.title, talk.user.full_name ]
+      end      
+    end
+    # send file to user
+    send_data csv.encode('Windows-1251'), type: 'text/csv; charset=windows-1251; header=present', disposition: "attachment; filename=tag_bundle_report.csv"
+  end
+
+  action_item :download_report, only: :show do
+    link_to 'Download Report', download_report_admin_tag_bundle_path(tag_bundle), method: 'get'
   end
 
 end
