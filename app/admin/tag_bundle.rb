276bdd143044788cdf1a264c8304257c2f82ab41
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2016-01-07 16:24:31 +0100

    select icon for tag bundle

--- app/admin/tag_bundle.rb
+++ app/admin/tag_bundle.rb
@@ -31,8 +31,8 @@ ActiveAdmin.register TagBundle do
   form do |f|
     f.inputs do
       f.input :group, collection: TagBundle::GROUPS
-      # f.input :icon, collection: ["arts", "tech", "media", "business",
-      # "science", "politics"]
+      f.input :icon, collection: ["arts", "tech", "media", "business",
+      "science", "politics"]
       f.input :promoted, label: 'Promoted (Promoted categories are listed on the home page.)'
       f.input :tag_list, input_html: { value: f.object.tag_list * ', ' }, label: "Tags"
       f.input :title_en
