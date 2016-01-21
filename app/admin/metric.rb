9e4bd88766a8bdc4a903af51449d13783ee11b60
phil <phil@branch14.org>
2016-01-21 16:50:16 +0100

    show 3 months instead of 1

--- app/admin/metric.rb
+++ app/admin/metric.rb
@@ -11,9 +11,9 @@ ActiveAdmin.register Metric do
   index as: ActiveAdmin::Views::IndexAsGraph
 
   controller do
-    # fetch all datapoints not older than 1 month
+    # fetch all datapoints not older than 3 months
     def scoped_collection
-      metrics = Metric.where('created_at > ?', 1.month.ago)
+      metrics = Metric.where('created_at > ?', 3.months.ago)
       metrics = Metric.all if metrics.empty? # for development
       metrics
     end
