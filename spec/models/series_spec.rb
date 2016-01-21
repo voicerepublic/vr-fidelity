5a53b597eb424b4bc6b15bb74398dbee9f477c38
phil <phil@branch14.org>
2016-01-21 11:57:55 +0100

    fix factory & spec

--- spec/models/series_spec.rb
+++ spec/models/series_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Series, type: :model do
 
   it 'updates description_html when updating the description' do
     s = FactoryGirl.create :series
-    expect(s.description_as_html).to be_empty
+    expect(s.description_as_html).to be_blank
     s.update_attribute :description, "test description list:\n* item 1\n* item 2"
     expect(s.description_as_html).to_not be_nil
   end
