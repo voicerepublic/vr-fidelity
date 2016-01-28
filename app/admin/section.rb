06e8c900293620328d709b7e1f06974abc47765f
phil <phil@branch14.org>
2016-01-28 14:33:19 +0100

    fix tag bundle export & make sections destroyable

--- app/admin/section.rb
+++ app/admin/section.rb
@@ -1,17 +1,19 @@
+# TODO introduce a mode, which can be automatic or manual
+# by default mode is set to automatic, then content is overwritten with content from sections.yml
+# on editing mode is set to manual
 ActiveAdmin.register Section do
 
   menu priority: 22
 
   permit_params :content
 
-  #actions :all, except: [:show, :new, :create, :destroy]
-  actions :index, :edit, :update
+  actions :index, :edit, :update, :destroy
 
   index do
     # selectable_column
     column :locale
     column :key
-    column :content
+    column :content # TODO truncate content
     column :updated_at
     actions
   end
