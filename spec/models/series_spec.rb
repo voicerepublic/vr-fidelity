3b8cd10dffb8be68e34deb23754a1ed2384c938e
Lino von Burg <linovonburg@gmail.com>
2016-07-27 15:11:51 +0200

    change series spec to reflect changes in validation

--- spec/models/series_spec.rb
+++ spec/models/series_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Series, type: :model do
 
   it 'updates description_html when updating the description' do
     s = FactoryGirl.create :series
-    expect(s.description_as_html).to be_blank
+    expect(s.description_as_html).to eq("<p>A description</p>\n")
     s.update_attribute :description, "test description list:\n* item 1\n* item 2"
-    expect(s.description_as_html).to_not be_nil
+    expect(s.description_as_html).to_not eq("<p>A description</p>\n")
   end
 
 end
