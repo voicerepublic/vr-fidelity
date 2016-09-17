3c2680a4f08ea00a22e91d9ad01e0ebc5556e932
phil <phil@branch14.org>
2016-09-17 11:44:46 +0200

    cleanup processing, introduce state `queued`

--- app/jobs/forward.rb
+++
@@ -1,5 +0,0 @@
-# this will be serialized to the db and resurected by the main app
-class Forward < Struct.new(:opts)
-  def perform
-  end
-end
--- app/jobs/postprocess.rb
+++
@@ -1,5 +0,0 @@
-# this will be serialized to the db and resurected by the main app
-class Postprocess < Struct.new(:opts)
-  def perform
-  end
-end
