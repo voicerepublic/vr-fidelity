0677942af1745b5ef822064aff3dc0f5f09a3f2d
phil <phil@branch14.org>
2017-03-30 08:54:33 +0200

    add strategy desilence

---
+++ lib/fidelity/strategy/desilence.rb
@@ -0,0 +1,42 @@
+module Fidelity
+  module Strategy
+    class Desilence < Base
+
+      class << self
+        def required_executables
+          %w( sox mv )
+        end
+      end
+
+      # http://digitalcardboard.com/blog/2009/08/25/the-sox-of-silence/
+      #
+      # cut silence at the beginning and end & reduce silence in
+      # between to 2 seconds if longer
+      def silence
+        "silence -l 1 0.2 1% -1 2.0 1% reverse silence 1 0.2 1% reverse"
+      end
+
+      def input
+        "#{name}.wav"
+      end
+
+      def run
+        desilence_wav
+        output
+      end
+
+      def output
+        input
+      end
+
+      def tmpfile
+        "#{name}-desilenced.wav"
+      end
+
+      def desilence_wav_cmd
+        "sox #{input} #{tmpfile} #{silence}; mv #{tmpfile} #{file}"
+      end
+
+    end
+  end
+end
